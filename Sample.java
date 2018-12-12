b8fee2971a6b8792dd5b5ff10653e0000b297b90
FIchsan <fiandasyahdan@gmail.com>
2018-12-12 15:07:13 +0700

    This is third commit

--- Sample.java
+++ Sample.java
@@ -1,5 +1,7 @@
 public class sample {
 	public static void main (String args[]){
-		system.out.println("This just a sample for git")
+		system.out.println("This just a sample for git");
+        	system.out.println("We are adding the output lines to see the changes");
+		system.out.println("This is second modifcation in java file to see the stage and unstaged changes");     
 	}
 }
